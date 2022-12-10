364c802e9bcbce36f13d980d163a8724b28f7d0a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-10 23:22:54 +0000

    Adding tarfile member sanitization to extractall()

--- src/data_utils.py
+++ src/data_utils.py
@@ -246,7 +246,26 @@ def get_open_subtitles_data_set(data_dir):
     print("Extracting tar file %s" % corpus_file)
     with tarfile.open(corpus_file, "r") as corpus_tar:
       # Extracts everything to a folder called OpenSubtitles. (assumption)
-      corpus_tar.extractall(data_dir)
+      def is_within_directory(directory, target):
+          
+          abs_directory = os.path.abspath(directory)
+          abs_target = os.path.abspath(target)
+      
+          prefix = os.path.commonprefix([abs_directory, abs_target])
+          
+          return prefix == abs_directory
+      
+      def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+      
+          for member in tar.getmembers():
+              member_path = os.path.join(path, member.name)
+              if not is_within_directory(path, member_path):
+                  raise Exception("Attempted Path Traversal in Tar File")
+      
+          tar.extractall(path, members, numeric_owner=numeric_owner) 
+          
+      
+      safe_extract(corpus_tar, data_dir)
 
   return process_open_subtitles_data_set(data_dir + '/OpenSubtitles2012', data_dir)
 
